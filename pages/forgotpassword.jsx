957a94994fc8a52daa528fc9c9900ea576fc1afe
Rafiu Islam <rafiuislam123@gmail.com>
2023-11-08 18:01:06 +0600

    2nd commit

--- pages/forgotpassword.jsx
+++ pages/forgotpassword.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import ForgetPassword from "../components/authComponents/Forgetpassword";
+// import ForgetPassword from "../components/authComponents/Forgetpassword";
 
 const forgotpassword = () => {
-  return (
-    <div>
-      <ForgetPassword />
-    </div>
-  );
+  return <div>{/* <ForgetPassword /> */}</div>;
 };
 
 export default forgotpassword;
